fce0e6f377e995903d6b15fcb133a684b9654f4e
Zeinab_Mahjoub <zmahjoub99@gmail.com>
2023-03-29 21:21:54 +0430

    feat: added Tailwindcss to the project

---
+++ app/Header.tsx
@@ -0,0 +1,16 @@
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+function Header() {
+  return (
+    <header>
+      <div className="grid grid-col-3 p-10 items-center">
+        <Bars3Icon className="h-8 w-8 cursor-pointer" />
+        <Link href="/" prefetch={false}>
+          <h1>The ZEINAB NEWS</h1>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
