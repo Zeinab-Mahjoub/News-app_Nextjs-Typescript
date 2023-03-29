987120ac3540da0931c48cd44de979d264498d57
Zeinab_Mahjoub <zmahjoub99@gmail.com>
2023-03-30 01:44:32 +0430

    checkpoint: fix the error in searchbox

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import SearchBox from "./SearchBox";
 function Header() {
   return (
     <header>
@@ -27,6 +28,7 @@ function Header() {
       {/* NavLinks */}
       <NavLinks />
       {/* SearchBox */}
+      <SearchBox />
     </header>
   );
 }
---
+++ app/SearchBox.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+function SearchBox() {
+  const [input, setInput] = useState("");
+  const router = useRouter;
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!input) return;
+    router.push(`/search?term=${input}`);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-6xl mx-auto flex justify-between items-center px-5"
+    >
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Search Keywords..."
+        className="flex-1 w-full h-14 rounded-sm placeholder-gray-500 text-gray-500 outline-none bg-transparent dark:text-orange-400"
+      />
+      <button
+        disabled={!input}
+        type="submit"
+        className="text-orange-400 disabled:text-gray-400"
+      >
+        Search
+      </button>
+    </form>
+  );
+}
+
+export default SearchBox;
